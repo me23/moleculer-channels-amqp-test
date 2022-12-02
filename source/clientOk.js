9f10a95748e22f279701a4049e4fc8095b9b95dc
Matthias Endres <git@me23.de>
2022-12-02 15:25:23 +0100

    clientOk.js added

---
+++ source/clientOk.js
@@ -0,0 +1,25 @@
+const { ServiceBroker } = require("moleculer");
+const ChannelsMiddleware = require("@moleculer/channels").Middleware;
+
+const conf = {
+    nodeID: "my-client",
+    disableBalancer: false,
+    transporter: 'amqp://localhost:5672',
+    middlewares: [
+        ChannelsMiddleware({
+            adapter: 'amqp://localhost:5672',
+        })
+    ]
+}
+
+
+    const broker = new ServiceBroker(conf);
+
+    broker.start()
+        .then(async () => {
+            console.log('start');
+            
+            await broker.sendToChannel('something.done', {no : 1});
+           broker.stop();
+        });
+
